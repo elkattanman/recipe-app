cec0e7c2f33eb13468ccbeb3294f14d50dcb711f
Mustafa Khaled <asdxasd012@gmail.com>
2021-01-04 23:45:33 +0200

    add Repositories

---
+++ src/main/java/com/elkattanman/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.elkattanman.repositories;
+
+import com.elkattanman.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+}
