6434836dd51b29fe117ea49391290124f297ead6
Mustafa Khaled <asdxasd012@gmail.com>
2021-01-05 02:44:35 +0200

    add index page and webjar for(bootstrap/jquery)

--- src/main/java/com/elkattanman/controllers/IndexController.java
+++ src/main/java/com/elkattanman/controllers/IndexController.java
@@ -1,13 +1,24 @@
 package com.elkattanman.controllers;
 
+import com.elkattanman.services.RecipeService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@AllArgsConstructor
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+        log.debug("Getting Index page");
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
